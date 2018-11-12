8c47bc9fd0aa6d2d98c824f87508ddf20bef122b
drewtonynguyen <andrew.nguyen@appfolio.com>
2018-11-12 15:29:29 -0800

    dn - allow re-rendering of day/week view upon resource change

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -80,6 +80,8 @@ export default class TimeGrid extends Component {
     ) {
       this.calculateScroll(nextProps)
     }
+
+    this.resources = Resources(nextProps.resources, nextProps.accessors)
   }
 
   gutterRef = ref => {
